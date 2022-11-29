5c9d1a17815fd8e0a7e7d570721449db7dbf758b
PrathmeshR <rathodprathmesh18@gmail.com>
2022-11-30 00:27:31 +0530

    update

--- tesla-clone/src/Components/Header.js
+++ tesla-clone/src/Components/Header.js
@@ -7,7 +7,21 @@ const Header = () => {
     return (  
         <div className="header">
             <div className="header_logo">
-              <img src={Teslalogo} alt=''/>
+              <img src={Teslalogo} alt='teslalogo'/>
+            </div>
+            
+            <div className="header_center">
+              <p>Model S</p>
+              <p>Model 3</p>
+              <p>Model X</p>
+              <p>Model Y</p>
+              <p>Solar Roof</p>
+              <p>Solar Panel</p>
+            </div>
+
+            <div className="header_right">
+              <p>Shop</p>
+              <p>Account</p>
             </div>
         </div>
      );
